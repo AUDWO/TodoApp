2131078bb37690c5f4d9766da442f9e34bd99bcf
audwo <kmjstj3@naver.com>
2023-08-02 01:01:44 +0900

    Create Board.js

---
+++ src/components/board/BoardItem.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+
+padding:16px;
+display:flex;
+justify-content:space-between;
+cursor:pointer;
+background-color:white;
+
+
+
+
+
+`
